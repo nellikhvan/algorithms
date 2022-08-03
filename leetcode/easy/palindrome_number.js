be3446980715e1f52e3323925a08dd2dfc2c43e8
khvannel <nelli.khvan@bistudio.com>
2022-08-03 10:22:58 +0200

    [Add] Leetcode| palindrome number

---
+++ leetcode/easy/palindrome_number.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ *
+ * (1) Get the number to check
+ * (2) Keep the number in a temporary variable
+ * (3) Reverse the number
+ * (4) Compare the temporary number with the inverted number
+ * (5) If the two numbers are the same, display “the number is a palindrome”
+ * (6) Otherwise, display “the number is not a palindrome”
+ *
+ * When we write logic of palindrome number our intention is to equalise original number with their reverse. For writing reverse number what we want is the individual digits of the original number .
+ * Now , original number %10 is the only logic by which we will get individial digits of a number in base 10.
+ * we will try to learn how we get individual digits ,
+ * Let number =12321.
+ * 1. number %10 = 1 (digit at ones place)
+ * 2. number /10 =1232.
+
+ * By repeating these two steps we get until
+ * number !=0 we get all digits of any number.
+ * When we calulate modulus of a number by any other number it will not give us the individual digits or a result which can't be implemented in our solution .
+ * so we use % 10 only in palindrome logic.
+ */
+function isPalindrome(x) {
+    if (x < 0) {
+        return false
+    }
+
+    const temp = x
+    let reversed = 0
+
+    while (x) {
+        reversed = (reversed * 10) + (x%10);
+        x = parseInt(x/10);
+    }
+    return reversed == temp;
+}
+
+let x = 242
+console.log(isPalindrome(x))
+
